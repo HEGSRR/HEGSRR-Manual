572bbafec711152ef5830ffdb517ce3ba623e322
doabell <35297086+doabell@users.noreply.github.com>
2023-07-18 10:59:05 -0400

    build: use bs4_book

---
+++ _common.R
@@ -0,0 +1,9 @@
+# example R options set globally
+# https://stackoverflow.com/a/66753995
+options(tidy.opts = list(width.cutoff = 60), tidy = TRUE)
+
+# example chunk options set globally
+knitr::opts_chunk$set(
+  comment = "#>",
+  collapse = TRUE
+  )
